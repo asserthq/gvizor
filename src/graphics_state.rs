453cbc15e497b5626b06d10d74e8f71f01002c43
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-05-12 21:22:14 +0700

    add func: create wgpu graphics state from window

--- src/graphics_state.rs
+++ src/graphics_state.rs
@@ -1,11 +1,40 @@
+use winit::window::Window;
+
 pub struct GraphicsState<'a> {
     surface: wgpu::Surface<'a>,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>
 }
 
 impl<'a> GraphicsState<'a> {
-    
+
+    pub async fn from_window(window: &'a Window) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+        let surface = instance.create_surface(window).unwrap();
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface)
+            }
+        ).await.unwrap();
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("Render Device"),
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default()
+            }, 
+            None
+        ).await.unwrap();
+        let size = window.inner_size();
+        let config = surface.get_default_config(&adapter, size.width, size.height).unwrap();
+
+        Self {
+            surface,
+            device,
+            queue,
+            config
+        }
+    }
 }
