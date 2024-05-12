d5a7d6308705759a9d843ed5956f46bca8279bad
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-05-12 18:50:52 +0700

    add Escape handler

--- src/app.rs
+++ src/app.rs
@@ -15,10 +15,18 @@ impl ApplicationHandler for GvizorApp {
         self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => self.window.as_ref().unwrap().request_redraw(),
+
+            WindowEvent::KeyboardInput { event, .. } => {
+                match event.state {
+                    ElementState::Pressed => event_loop.exit(),
+                    _ => ()
+                }
+            }
+
             _ => ()
         }
     }
